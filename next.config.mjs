7ab501998a479145832e756a57b50790bba1fa08
inspireena <saini.reena962@gmail.com>
2024-09-24 10:25:57 +0530

    fixed deployement for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-    basePath: isProd ? '/portfolio' : '', 
-    assetPrefix: isProd ? '/portfolio/' : '',
-    trailingSlash: true,
-    output: 'export',
-    images: { unoptimized: true }
+    // basePath: isProd ? '/portfolio' : '', 
+    // assetPrefix: isProd ? '/portfolio/' : '',
+    // trailingSlash: true,
+    // output: 'export',
+    // images: { unoptimized: true }
 };
 
 export default nextConfig;
